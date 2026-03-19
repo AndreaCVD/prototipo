41f8ed7a546cdfc8f4eaf73feb7b658dbbcafdf5
AinaMC <aina.mestres@estudiantat.upc.edu>
2026-03-19 16:35:04 +0100

    stats combate
    estilo pokemon de combate siguiendo un tutorial, hay que meter cada personaje 1 a 1 con sus estats y modelo

---
+++ Assets/Scripts/Parameters.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+//para indentificar que stat es
+public enum PersonajesStats
+{
+    Fuerza,
+    Inteligencia,
+    Carisma,
+    Constitucion
+}
+
+//para poder editarlo en el editor de unity le ponemos serializable
+[Serializable]
+public class ValueContainer
+{
+    public int value;
+    public PersonajesStats stats;
+
+    public ValueContainer (int value, PersonajesStats stats)
+    {
+        this.value = value;
+        this.stats = stats;
+    }
+}
+[Serializable]
+public class ValueBlock
+{
+    private const int persStatsNum = 4;
+    public List<ValueContainer> values;
+    public void InitPersonaje()
+    {
+        values = new List<ValueContainer>();
+        for (int i = 0; i < persStatsNum; i++)
+        {
+            values.Add(new ValueContainer(0, (PersonajesStats)i));
+        }
+    }
+}
+
+[CreateAssetMenu(menuName = "Data/Pers")]
+public class Parameters : ScriptableObject
+{
+    public string namePers;
+    public ValueBlock stats;
+
+    public GameObject modelPrefab;
+
+    [ContextMenu("Init")]
+    public void Init()
+    {
+        stats = new ValueBlock();
+        stats.InitPersonaje();
+    }
+}

67bee9748b570a365068bb1fb7e1c5538960fa8c
AinaMC <aina.mestres@estudiantat.upc.edu>
2026-03-22 17:58:10 +0100

    combatUI_command panel

---
+++ Assets/Scripts/Combat/Combat_UI/CommandPanel.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class CommandPanel : MonoBehaviour
+{
+    //Boton Fuerza
+    public void Fuerza()
+    {
+        Debug.Log("Fuerza");
+    }
+    //Boton Inteligencia
+    public void Intel()
+    {
+        Debug.Log("Inteligencia");
+    }
+    //Boton Carisma
+    public void Carisma()
+    {
+        Debug.Log("Carisma");
+    }
+    //Boton Huir
+    public void Huir()
+    {
+        Debug.Log("Huir");
+    }
+}

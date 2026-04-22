5087eb912cf1d97cb1c8aa40ce7ec84ceb1114e1
AinaMC <aina.mestres@estudiantat.upc.edu>
2026-04-23 01:35:57 +0200

    puzzle hecho

--- Assets/Scripts/Puzzles/EmpujarObjetos.cs
+++ Assets/Scripts/Puzzles/EmpujarObjetos.cs
@@ -7,9 +7,14 @@ public class EmpujarObjetos : MonoBehaviour
     private bool enMovimiento = false;
     private Vector3 destino;
 
+    private LayerMask layerMask;
+    private bool puzzlefinished;
+
     void Start()
     {
         destino = transform.position; // Posición inicial
+        puzzlefinished = false;
+        layerMask = 1 << LayerMask.NameToLayer("FinPuzzle");
     }
 
     void Update()
@@ -30,29 +35,50 @@ public class EmpujarObjetos : MonoBehaviour
 
     void OnCollisionEnter(Collision col)
     {
-        // Si el jugador choca con este objeto
-        if (col.gameObject.CompareTag("Player") && !enMovimiento)
+        // Si el jugador choca con este objeto y el puzzle no esta completado
+        if (col.gameObject.CompareTag("Player") && !enMovimiento && !puzzlefinished)
         {
             // Dirección del empuje (basada en la posición del jugador)
             Vector3 direccion = (transform.position - col.transform.position).normalized;
 
             //o se mueve en X o en Y, no en diagonal
-            
 
             // Redondear dirección a ejes principales (para moverse en grid)
-            direccion = new Vector3( Mathf.Round(direccion.x), 0f, Mathf.Round(direccion.z)
-            );
+            direccion = new Vector3(Mathf.Round(direccion.x), 0f, Mathf.Round(direccion.z));
 
             // Calcular nueva posición
             Vector3 nuevaPos = transform.position + direccion * distanciaCasilla;
 
             //Comprovar tmb si esta el final del puzzle, que se mueva igual
-            // Comprobar si hay algo en la nueva posición 
+            // Comprobar si hay algo en la nueva posición --> da TRUE o FALSE
             if (!Physics.CheckBox(nuevaPos, Vector3.one * 0.4f))
             {
                 destino = nuevaPos;
                 enMovimiento = true;
+                //LayerMask layer;
+
+            }
+            else //Y si delante esta el final del puzzle
+            {
+                Collider[] hit = Physics.OverlapBox(nuevaPos, Vector3.one * 0.4f, Quaternion.identity, layerMask);
+                //Para ver el tag:
+                Debug.Log("AAAAAAA");
+                foreach (var colision in hit)
+                {
+                    if ( colision.gameObject.CompareTag("FinalPuzzle"))
+                    {
+                        Debug.Log("El puzzle se ha completado");
+                        destino = nuevaPos;
+                        enMovimiento = true;
+                        puzzlefinished = true;
+                    }
+
+                }
+                //Physics.OverlapBox --> saber que obj hay
+                // centro, halfExtens, orientacion, layerMask
+
             }
         }
+
     }
 }

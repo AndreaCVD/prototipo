05a210d3c03131ceb7e5268feb5fe7da887ec734
AinaMC <aina.mestres@estudiantat.upc.edu>
2026-04-25 01:40:32 +0200

    dialogo funciona
    falta ounicarse con el desde el puzzle y interaccion

--- Assets/Dialogos/TestDialog.cs
+++ Assets/Dialogos/TestDialog.cs
@@ -1,12 +1,27 @@
+using System.Collections.Generic;
 using UnityEngine;
 
-public class TestDialog : MonoBehaviour
+namespace cherrydev
 {
-    [SerializeField]  DialogBehaviour dialogBehaviour;
-    [SerializeField]  DialogNodeGraph dialogGraph;
-
-    private void Start()
+    public class TestDialog : MonoBehaviour
     {
-        dialogBehaviour.StartDialog(dialogGraph);
+        [Header("EL PREFAB")]
+        [SerializeField] private DialogBehaviour _dialogBehaviour;
+        [Header("DIALOGOS PERSONAJES")]
+        [SerializeField] List<DialogNodeGraph> Viejo = new List<DialogNodeGraph>();
+        [SerializeField] List<DialogNodeGraph> Nim = new List<DialogNodeGraph>();
+        [SerializeField] List<DialogNodeGraph> Etkis = new List<DialogNodeGraph>();
+        [SerializeField] List<DialogNodeGraph> Lerendur = new List<DialogNodeGraph>();
+        [SerializeField] List<DialogNodeGraph> Libro = new List<DialogNodeGraph>();
+
+
+        //La conversa, podemos tener todas las conversas guardadas y enviar la que se necesite
+        [SerializeField] private DialogNodeGraph dialogGraph;
+
+        //Para que el dialogo se active necesitamos esto:
+        private void Start()
+        {
+            _dialogBehaviour.StartDialog(dialogGraph);
+        }
     }
 }
---
+++ Assets/Dialogos/no_Dialog Behaviour.cs
@@ -0,0 +1,26 @@
+using System.Diagnostics;
+using UnityEngine;
+
+public class DialogBehaviour : MonoBehaviour
+{
+
+
+    //public void StartDialog(DialogNodeGraph dialogNodeGraph)
+    //{
+    //    isDialogStarted = true;
+
+    //    if (dialogNodeGraph.nodesList == null)
+    //    {
+    //        Debug.LogWarning("Dialog Graph's node list is empty");
+    //        return;
+    //    }
+
+    //    onDialogStarted?.Invoke();
+
+    //    currentNodeGraph = dialogNodeGraph;
+
+    //    DefineFirstNode(dialogNodeGraph);
+    //    CalculateMaxAmountOfAnswerButtons();
+    //    HandleDialogGraphCurrentNode(currentNode);
+    //}
+}

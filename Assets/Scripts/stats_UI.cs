630dee9a1d1d20cbb680b241060a7c9127aa6037
AndreaCVD <bluecarolina10@gmail.com>
2026-05-27 02:26:25 +0200

    inventary UI acabado

--- Assets/Scripts/stats_UI.cs
+++ Assets/Scripts/stats_UI.cs
@@ -1,7 +1,7 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
+using UI = UnityEngine.UI;
 using UnityEngine.UIElements; // Imprescindible para UI Toolkit
 using System.Linq; //Comparacion de Listas
 
@@ -13,40 +13,52 @@ public class stats_UI : MonoBehaviour
     //ref del UI
     private VisualElement root;
     private IntegerField fieldFUE, fieldINT, fieldCAR, fieldLIFE;
-    public bool canvas_open;
-    public CanvasGroup canvas_inventario;
-
     private VisualElement heartFill;
     private int maxLife;
 
+    //inv en el UI
+    private VisualElement inventoryGrid;
+    private Button btnInventory;
+
+    //contadores inventory
+    private int llaves;
+    private int llaveMaestra;
+    private int daga;
+    private int espada;
+    private int pocionVida;
+
+    //lo del canva para quitar en el futuro
     [Header("Inventario")]
     [SerializeField] TMP_Text llave_text;
     [SerializeField] TMP_Text llaveMaestra_text;
     [SerializeField] TMP_Text daga_text;
     [SerializeField] TMP_Text espada_text;
     [SerializeField] TMP_Text pocionVida_text;
+    public bool canvas_open;
+    public CanvasGroup canvas_inventario;
 
-    // [SerializeField] TMP_Text texto_inventario;
-    //[SerializeField] GameObject prefabElemento; // Arrastra el Text prefab aquí en el inspector
-    //public Transform contenedor; // Arrastra el "ContenedorLista" del Canvas aquí
-    
-    private int llaves;
-    private int llaveMaestra;
-    private int daga;
-    private int espada;
-    private int pocionVida;
-    // var listaX;
     private void OnEnable()
     {
         var uiDocument = GetComponent<UIDocument>();
         root = uiDocument.rootVisualElement;
 
+        //stats
         fieldFUE = root.Q("FUE").Q<IntegerField>();
         fieldINT = root.Q("INT").Q<IntegerField>();
         fieldCAR = root.Q("CAR").Q<IntegerField>();
-
         fieldLIFE = root.Q("int_life").Q<IntegerField>();
         heartFill = root.Q<VisualElement>("heart-fill");
+
+        //inventary
+        inventoryGrid = root.Q<VisualElement>("inventory-grid");
+        btnInventory = root.Q<Button>("btn-inventory");
+        btnInventory.clicked += ToggleInventary;
+
+        inventoryGrid.style.display = DisplayStyle.None;
+    }
+    private void OnDisable()
+    {
+        btnInventory.clicked -= ToggleInventary;
     }
 
     void Start()
@@ -57,8 +69,8 @@ public class stats_UI : MonoBehaviour
         daga = 0;
         pocionVida = 0;
 
-        canvas_open = true;
-        //abrirInventario();
+        canvas_open = false;
+
         //Seteamos valores, int -> string
         int f = protagonista.stats.Get(PersonajesStats.Fuerza);
         int i = protagonista.stats.Get(PersonajesStats.Inteligencia);
@@ -69,29 +81,41 @@ public class stats_UI : MonoBehaviour
         maxLife = protagonista.stats.Get(PersonajesStats.Constitucion);
 
         SetFuerza(f);
-
         SetIntel(i);
-
         SetCarisma(c);
-
         SetInventario();
-        //aux = new List<int>(protagonista.Inventario);
+
     }
 
     void Update()
     {
+
+        //health
         int aux = protagonista.stats.Get(PersonajesStats.Constitucion);
         if (fieldLIFE.value != aux)
         {
             SetConstitucion(aux);
         }
 
-        bool x = areListEqual();
-        if (!x)
+        //inventary
+        if (!areListEqual())
         {
             SetInventario();
         }
+
+        //abrir/cerrar con la tecla I
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            ToggleInventary();
+        }
     }
+
+    void ToggleInventary()
+    {
+        bool isDisplayed = inventoryGrid.style.display == DisplayStyle.Flex;
+        inventoryGrid.style.display = isDisplayed ? DisplayStyle.None : DisplayStyle.Flex;
+    }
+
     bool areListEqual()
     {
         //Si  no son iguales de largo entonces estaran mal 100%
@@ -135,25 +159,12 @@ public class stats_UI : MonoBehaviour
         daga = protagonista.Inventario.Daga.Count();
         espada = protagonista.Inventario.Espada.Count();
 
+        /*para eliminar
         llave_text.text = "Llaves = " + llaves.ToString();
         llaveMaestra_text.text = "Llave Maestra = " + llaveMaestra.ToString();
         pocionVida_text.text = "Pocion = " + pocionVida.ToString();
         daga_text.text = "Daga = " + daga.ToString();
         espada_text.text = "Espada = " + espada.ToString();
-    }
-    public void abrirInventario()
-    {
-        //Debug.Log("se esta cerrando?");
-        //if (!canvas_open)
-        //{
-        //    canvas_open = true;
-        //    canvas_inventario.alpha = Mathf.Lerp(0f, 1f, 5f);
-        //}
-        //else
-        //{
-        //    canvas_open = false;
-        //    canvas_inventario.alpha = Mathf.Lerp(0f, 0f, 5f);
-        //}
-
+        */
     }
 }

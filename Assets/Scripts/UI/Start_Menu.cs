c52a71ddba93b9dfc7a327511a5b8b865508dac6
AndreaCVD <bluecarolina10@gmail.com>
2026-06-03 21:59:43 +0200

    fix de la cuerda y menu acabado

--- Assets/Scripts/UI/Start_Menu.cs
+++ Assets/Scripts/UI/Start_Menu.cs
@@ -1,49 +1,75 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Start_Menu : MonoBehaviour
 {
     [Header("Obj escena")]
     public Puzzle lista;
-    [Header("Obj escena")]
-    [SerializeField] GameObject tutorial_obj;
-    [SerializeField] GameObject nivel_1_obj;
-    [SerializeField] GameObject nivel_2_obj;
-    [SerializeField] GameObject nivel_3_obj;
-    [Header("Materiales")]
-    [SerializeField] Material tutorial_m;
-    [SerializeField] Material nivel_1_m;
-    [SerializeField] Material nivel_2_m;
-    [SerializeField] Material nivel_3_m;
-    [SerializeField] Material bloqueado_m;
+
+    [Header("Sprites")]
+    [SerializeField] Sprite tutorial_spr;
+    [SerializeField] Sprite nivel_1_spr;
+    [SerializeField] Sprite nivel_2_spr;
+    [SerializeField] Sprite nivel_3_spr;
+    [SerializeField] Sprite bloqueado_spr_1;
+    [SerializeField] Sprite bloqueado_spr_2;
+    [SerializeField] Sprite bloqueado_spr_3;
+
+
+    //[Header("Obj escena")]
+    //[SerializeField] GameObject tutorial_obj;
+    //[SerializeField] GameObject nivel_1_obj;
+    //[SerializeField] GameObject nivel_2_obj;
+    //[SerializeField] GameObject nivel_3_obj;
+    //[Header("Materiales")]
+    //[SerializeField] Material tutorial_m;
+    //[SerializeField] Material nivel_1_m;
+    //[SerializeField] Material nivel_2_m;
+    //[SerializeField] Material nivel_3_m;
+    //[SerializeField] Material bloqueado_m;
 
     void Start ()
     {
-        tutorial_obj.GetComponent<Renderer>().material = tutorial_m;
-        if (lista.NivelDesbloqueado[0].acabado)
-        {
-            nivel_1_obj.GetComponent<Renderer>().material = nivel_1_m;
-        }
-        else
-        {
-            nivel_1_obj.GetComponent<Renderer>().material = bloqueado_m;
-        }
+        var root = GetComponent<UIDocument>().rootVisualElement;
 
-        if (lista.NivelDesbloqueado[1].acabado)
-        {
-            nivel_2_obj.GetComponent<Renderer>().material = nivel_2_m;
-        }
-        else
-        {
-            nivel_2_obj.GetComponent<Renderer>().material = bloqueado_m;
-        }
+        root.Q<VisualElement>("tutorial").style.backgroundImage = new StyleBackground(tutorial_spr);
 
-        if (lista.NivelDesbloqueado[2].acabado)
-        {
-            nivel_3_obj.GetComponent<Renderer>().material = nivel_3_m;
-        }
-        else
-        {
-            nivel_3_obj.GetComponent<Renderer>().material = bloqueado_m;
-        }
+        root.Q<VisualElement>("nivel_1").style.backgroundImage = new StyleBackground(
+            lista.NivelDesbloqueado[0].acabado ? nivel_1_spr : bloqueado_spr_1);
+
+        root.Q<VisualElement>("nivel_2").style.backgroundImage = new StyleBackground(
+            lista.NivelDesbloqueado[1].acabado ? nivel_2_spr : bloqueado_spr_2);
+
+        root.Q<VisualElement>("nivel_3").style.backgroundImage = new StyleBackground(
+            lista.NivelDesbloqueado[2].acabado ? nivel_3_spr : bloqueado_spr_3);
+
+
+    //tutorial_obj.GetComponent<Renderer>().material = tutorial_m;
+    //if (lista.NivelDesbloqueado[0].acabado)
+    //{
+    //    nivel_1_obj.GetComponent<Renderer>().material = nivel_1_m;
+    //}
+    //else
+    //{
+    //    nivel_1_obj.GetComponent<Renderer>().material = bloqueado_m;
+    //}
+
+    //if (lista.NivelDesbloqueado[1].acabado)
+    //{
+    //    nivel_2_obj.GetComponent<Renderer>().material = nivel_2_m;
+    //}
+    //else
+    //{
+    //    nivel_2_obj.GetComponent<Renderer>().material = bloqueado_m;
+    //}
+
+    //if (lista.NivelDesbloqueado[2].acabado)
+    //{
+    //    nivel_3_obj.GetComponent<Renderer>().material = nivel_3_m;
+    //}
+    //else
+    //{
+    //    nivel_3_obj.GetComponent<Renderer>().material = bloqueado_m;
+    //}
     }
 }
